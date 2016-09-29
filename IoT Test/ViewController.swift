362854433281ea3db6d8c011a30d7fb62ce5e547
Tom Guarnery <thomas.guarnery@gmail.com>
2016-09-29 16:22:48 -0400

    Initial Commit

---
+++ IoT Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IoT Test
+//
+//  Created by Thomas Guarnery on 9/29/16.
+//  Copyright © 2016 Tom Guarnery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
